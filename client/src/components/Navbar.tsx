552266be45f1698aa9ce2699d58e3d0942719dd3
Nura730 <arunshanmugavel3726@gmail.com>
2026-06-11 18:41:55 +0530

    Banner and navbar Completed

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { BikeIcon } from "lucide-react";
+import { BikeIcon, ChevronDownIcon } from "lucide-react";
 import { SearchIcon } from "lucide-react";
 import { ShoppingCartIcon } from "lucide-react";
+import { UserIcon, XIcon, MenuIcon, PackageIcon, MapPinIcon, ArrowUpRightIcon, ShieldIcon, LogOutIcon} from "lucide-react";
 
 const Navbar = () => {
   const user: any = {
-    name: "John Doe",
-    email: "john@example.com",
+    name: "Nura",
+    email: "@example.com",
     isAdmin: true,
   };
 
-  const { cartCount, setisCartOpen } = {
+  const { cartCount, setIsCartOpen } = {
     cartCount: 5,
     setIsCartOpen: (_data: any) => {},
   };
@@ -19,6 +20,20 @@ const Navbar = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [userMenuOpen, setUserMenuOpen] = useState(false);
   const navigate = useNavigate();
+
+  const handleSearch = (e: React.SubmitEvent) => {
+    e.preventDefault()
+    if(searchQuery.trim()) {
+        navigate(`/search?q=${encodeURIComponent(searchQuery.trim())}`)
+        setSearchQuery("")
+    }
+  }
+
+  const handleLogout = () => {
+    setUserMenuOpen(false)
+    navigate("/");
+  }
+
   return (
     <nav className="bg-white sticky top-0 z-50 border-b border-app-border">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16 gap-4">
@@ -40,7 +55,7 @@ const Navbar = () => {
             </Link>
           </div>
           {/*Searchbar*/}
-          <form className="hidden sm:flex flex-1 max-w-sm text-xs sm:tex-sm">
+          <form onSubmit={handleSearch} className="hidden sm:flex flex-1 max-w-sm text-xs sm:tex-sm">
             <div className="relative w-full">
               <SearchIcon
                 className="absolute 
@@ -60,15 +75,15 @@ const Navbar = () => {
           <div className="flex items-center gap-3">
             {/*cart*/}
             <button
-              className="realtive p-2 rounded-xl"
-              onClick={() => setisCartOpen(true)}
+              className="relative p-2 rounded-xl"
+              onClick={() => setIsCartOpen(true)}
             >
               <ShoppingCartIcon className="size-5 text-zinc-900" />
               {cartCount > 0 && (
                 <span
                   className="absolute -top-1 -right-1 
             size-4 bg-app-orange text-white text-[10px] rounded-full 
-            flex-center"
+            flex items-center justify-center"
                 >
                   {cartCount}
                 </span>
@@ -77,14 +92,68 @@ const Navbar = () => {
             {/*user*/}
             <div className="relative">
               {user ? (
-              <button className="flex items-center gap-2 p-2">
-                <div className="size-7 rounded-full bg-green-950 text-white flex-center">{user.name.charAt(0).toUpperCase()}
-                    
+                <button onClick={()=> setUserMenuOpen(!userMenuOpen)} className="flex items-center gap-2 p-2">
+                  <div className="size-7 rounded-full bg-green-950 text-white flex items-center justify-center">
+                    {user.name.charAt(0).toUpperCase()}
+                  </div>
+                  <ChevronDownIcon className="size-3 text-zinc-500" />
+                </button>
+              ) : (
+                <div className="flex-center gap-2">
+                  <Link
+                    to="/login"
+                    className="hidden md:flex items-center gap-2 px-4 py-2 text-sm font-medium text-white bg-green-950 rounded-full hover:bg-green-950-light transition-colors"
+                  >
+                    <UserIcon size={16} /> Sign In
+                  </Link>
+                  {userMenuOpen ? (
+                    <XIcon
+                      className="md:hidden"
+                      onClick={() => setUserMenuOpen(!userMenuOpen)}
+                    />
+                  ) : (
+                    <MenuIcon
+                      className="md:hidden"
+                      onClick={() => setUserMenuOpen(!userMenuOpen)}
+                    />
+                  )}
                 </div>
-              </button>
-              ) : 
-              (
-              <div></div>
+              )}
+
+              {userMenuOpen && (
+                <>
+                  <div
+                    className="fixed inset-0 z-40"
+                    onClick={() => setUserMenuOpen(false)}
+                  >
+                    <div className="absolute right-0 mt-2.5 w-56 bg-white rounded-xl shadow-lg border border-app-border py-2 z-50 animate-fade-in">
+                        {user && (
+                            <div className="px-4 py-2 border-b border-app-border">
+                                <p className="text-sm font-medium text-zinc-900">{user?.name}</p>
+                                <p className="text-xs text-zinc-500">{user?.email}</p>
+                            </div>
+                        )}
+                        <div onClick={()=> setUserMenuOpen(false)}>
+                            {!user && <Link to='/login' className="dropdown-link"><UserIcon size={16}/>Sign In</Link>}
+
+                            {user && <Link to='/orders' className="dropdown-link"><PackageIcon size={16}/>My Orders</Link>}
+                            {user && <Link to='/addresses' className="dropdown-link"><MapPinIcon size={16}/>Addresses</Link>}
+                            <Link to='/products' className="dropdown-link md:hidden"><ArrowUpRightIcon size={16}/>Products</Link>
+                            <Link to='/deals' className="dropdown-link"><ArrowUpRightIcon size={16}/>Deals</Link>
+                            {user?.isAdmin && (
+                                <Link to='/admin/products' className="dropdown-link"><ShieldIcon className="text-app-orange-dark" size={16}/><span className="text-app-orange-dark">Admin Panel</span></Link>
+                            )}
+                            {user && (
+                                <div className="border-t border-app-border pt-1">
+                                    <button onClick={handleLogout} className="flex items-center gap-3 px-4 py-2.5 text-sm text-app-error hover:bg-red-50 w-full transition-colors">
+                                        <LogOutIcon size={16}/>Logout
+                                    </button>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                  </div>
+                </>
               )}
             </div>
           </div>
