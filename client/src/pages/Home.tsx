a8846097169ee1d7c6e765b35f04165b9ff0a927
Nura730 <arunshanmugavel3726@gmail.com>
2026-06-22 21:56:13 +0530

    Home page completed

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Features from "../components/Home/Features";
 import HomeCategories from "../components/Home/HomeCategories";
 import PopularProducts from "../components/Home/PopularProducts";
 import AppPromoBanner from "../components/Home/AppPromoBanner";
+import Newsletter from "../components/Home/Newsletter";
 const Home = () => {
   return <div className="min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
 
@@ -11,6 +12,7 @@ const Home = () => {
     <HomeCategories />
     <PopularProducts />
     <AppPromoBanner />
+    <Newsletter />
   </div>;
 };
 
