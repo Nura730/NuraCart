fdbd12542448e91e5a8fa6af5b3006a0eb5d39e4
Nura730 <arunshanmugavel3726@gmail.com>
2026-07-01 21:10:11 +0530

    Finished untill removecart

---
+++ client/src/context/CartContext.tsx
@@ -0,0 +1,57 @@
+import { createContext, useContext, useEffect, useState, type ReactNode } from "react";
+import type { CartItem, Product } from "../types";
+
+
+interface CartContextType {
+    items: CartItem[];
+    addToCart: (product: Product, qunatity?: number)=> void;
+    removeFromCart: (productId: string) => void;
+    updateQuantity: (ProductId: string, qunatity: number) => void;
+    clearCart: () => void;
+    cartCount: number;
+    cartTotal: number;
+    isCartOpen: boolean;
+    setIsCartOpen: (open: boolean) => void;
+}
+
+const CartContext = createContext<CartContextType | undefined>(undefined)
+
+export function CartProvider({children} : {children: ReactNode}) {
+
+
+    const [items, setItems] = useState<CartItem[]>(()=> {
+        const saved = localStorage.getItem("app_cart")
+        return saved ? JSON.parse(saved) : []
+    })
+
+    const [isCartOpen, setIsCartOpen] = useState(false)
+
+    useEffect(()=>{
+        localStorage.setItem("app_cart",JSON.stringify(items))
+    },[items])
+
+    const addToCart = (product: Product, quantity = 1)=>{
+        setItems((prev)=>{
+            const existing = prev.find((item) => item.product._id === product._id)
+            if(existing) {
+                return prev.map((item)=(items.product,_id === product._id ? {...items, quantity: items.quantity + quantity} : item))
+            }
+            return [...prev, {product, quantity}]
+        })
+        setIsCartOpen(true)
+    }
+
+    const removeFromCart = (productId: string) => {
+        setItems((prev) => prev.filter((item) => item.product._id !== productId) );
+    }
+
+    return <CartContext.Provider value={{}}>
+        {children}
+    </CartContext.Provider>
+}
+
+export function useCart() {
+    const context = useContext(CartContext)
+    if(!context) throw new Error("useCart must be used within CartProvider")
+        return context;
+}
